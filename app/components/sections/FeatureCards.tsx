fd0f01e58a9d0b650ef5f88672f187354219d64c
Tulasi Shukla <tulsishuklag@gmail.com>
2026-05-08 16:29:41 +0530

    Reduce image dimensions in FeatureCards component

--- app/components/sections/FeatureCards.tsx
+++ app/components/sections/FeatureCards.tsx
@@ -60,8 +60,8 @@ const FeatureCards: React.FC = () => {
               <Image
                 src={item.image}
                 alt={item.title}
-                width={400}
-                height={400}
+                width={300}
+                height={300}
                 className="w-full h-auto object-cover block shadow-sm"
               />
             </motion.div>
